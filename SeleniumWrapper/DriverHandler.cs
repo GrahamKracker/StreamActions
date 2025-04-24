5174a90b9ab7597d5cfa6c68bcc0fdc860fb5387
GrahamKracker <gjguzynski@gmail.com>
2025-04-24 11:38:08 -0400

    improve driver version handling and update mod description

--- SeleniumWrapper/DriverHandler.cs
+++ SeleniumWrapper/DriverHandler.cs
@@ -1,6 +1,6 @@
 ﻿using System.Diagnostics;
 using System.IO.Compression;
-using System.Text.Json;
+using Newtonsoft.Json;
 
 namespace SeleniumWrapper;
 
@@ -10,17 +10,20 @@ public class DriverHandler
     {
         using HttpClient client = new();
 
-        dynamic json = JsonSerializer.Deserialize<dynamic>(
-            client.GetStringAsync("https:// github.com/GrahamKracker/StreamActions/remotevars.json").Result);
+        dynamic? json = JsonConvert.DeserializeObject(
+            client.GetStringAsync("https://raw.githubusercontent.com/GrahamKracker/StreamActions/refs/heads/main/remotevars.json").Result);
 
         var driverExecutable = Path.Combine(cacheFolder, "edgedriver_win64", "msedgedriver.exe");
 
         var fileVersionInfo = FileVersionInfo.GetVersionInfo(driverExecutable);
 
-        if (!File.Exists(driverExecutable) || fileVersionInfo.FileVersion != json?.driverVersion)
+        if (!File.Exists(driverExecutable) || fileVersionInfo.FileVersion != json?.driverVersion.ToString())
         {
             if(json == null)
                 throw new InvalidOperationException("Failed to get driver version from github");
+
+            if(Directory.Exists(Path.Combine(cacheFolder, "edgedriver_win64")))
+                Directory.Delete(Path.Combine(cacheFolder, "edgedriver_win64"), true);
 
             var response =
                 client.GetAsync($"https://msedgedriver.azureedge.net/{json.driverVersion}/edgedriver_win64.zip").Result;
@@ -35,7 +38,7 @@ public class DriverHandler
             //extract the zip
 
             ZipFile.ExtractToDirectory(Path.Combine(cacheFolder, "edgedriver_win64.zip"),
-                Path.Combine(cacheFolder, "edgedriver_win64"));
+                Path.Combine(cacheFolder, "edgedriver_win64"), true);
 
             File.Delete(Path.Combine(cacheFolder, "edgedriver_win64.zip"));
         }
